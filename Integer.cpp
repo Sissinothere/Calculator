0bc73db01980037a72631784b0b6233595bdb77e
Xixi <flyskyxi@gmail.com>
2014-04-11 12:10:31 -0400

    For integer class.

---
+++ Integer.cpp
@@ -0,0 +1,17 @@
+/*
+ * Integer.cpp
+ *
+ *  Created on: Apr 11, 2014
+ *      Author: Sissi
+ */
+
+#include "Integer.h"
+
+Integer::Integer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Integer::~Integer() {
+	// TODO Auto-generated destructor stub
+}
---
+++ Integer.h
@@ -0,0 +1,17 @@
+/*
+ * Integer.h
+ *
+ *  Created on: Apr 11, 2014
+ *      Author: Sissi
+ */
+
+#ifndef INTEGER_H_
+#define INTEGER_H_
+
+class Integer {
+public:
+	Integer();
+	virtual ~Integer();
+};
+
+#endif /* INTEGER_H_ */
