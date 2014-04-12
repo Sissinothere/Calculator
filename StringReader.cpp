873c5050bf91a3b67d0e2e1cc76e9ef2a7e3c549
JohnnyRacket <john.randolph.harrison@gmail.com>
2014-04-12 19:45:20 -0400

    fixes for testing to not crash others

--- StringReader.cpp
+++ StringReader.cpp
@@ -126,7 +126,7 @@ for (int i =0;i < input.length();i++){
 
 				//test if there is anything except 0-9, if there is, add square brackets
 				//if there is sqaure brackets you test if there is anything to distribute
-
+/*
 				if (nbr->ansIsComplex()){
 					stringstream cp;
 					cp << '[';
@@ -140,7 +140,7 @@ for (int i =0;i < input.length();i++){
 					}
 				}
 
-
+*/
 				input.insert(endPos+1,nbr->getFinalAnswer());
 
 
